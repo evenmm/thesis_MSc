d9cfb5f8b270ee16bcbdad2ac10236d3b72f64b6
evenmm <even.myklebust@gmail.com>
2020-02-20 21:09:59 +0100

    Save as is 21:09 thursdaye

--- head-direction-inference.py
+++ head-direction-inference.py
@@ -18,17 +18,13 @@ numpy.random.seed(13)
 ##############
 offset = 1000 # Starting point in observed X values
 T = 1000
-sigma_fit = 8 # Variance for the GP that is fitted
-delta_fit = 0.3 # Scale for the GP that is fitted
-sigma_epsilon_fit = 0.2 # Assumed variance of observations for the GP that is fitted
-X_dim = 40 # Number of grid points
-TOLERANCE = .01 # for Newton's method
-
-"""
-sigma_Kx = 8 # variance of kx
-delta_Kx = 0.3 # length scale of kx
-Kx = Kx + np.identity(T)*10e-5 # To be able to invert Kx we add a small amount on the diagonal
-"""
+sigma_fit = 8 # Variance for the GP that is fitted. 8
+delta_fit = 0.3 # Scale for the GP that is fitted. 0.3
+sigma_epsilon_fit = 0.2 # Assumed variance of observations for the GP that is fitted. 10e-5
+X_dim = 50 # Number of grid points
+TOLERANCE_f = 1 # for Newton's method
+TOLERANCE_X = 0.1 # for X posterior
+LIKELIHOOD = "bernoulli"
 
 ##############################
 # Data fetch and definitions #
@@ -36,7 +32,6 @@ Kx = Kx + np.identity(T)*10e-5 # To be able to invert Kx we add a small amount o
 
 name = sys.argv[1] #'Mouse28-140313_stuff_BS0030_awakedata.mat'
 
-sigma = 10 # window for smoothing
 thresholdforneuronstokeep = 1000 # number of spikes to be considered useful
 
 mat = scipy.io.loadmat(name)
@@ -45,7 +40,7 @@ cellspikes = array(mat['cellspikes'])
 cellnames = array(mat['cellnames'])
 trackingtimes = ravel(array(mat['trackingtimes']))
 
-## make matrix of spikes
+## make matrix of spikes y_spikes
 startt = min(trackingtimes)
 binsize = mean(trackingtimes[1:]-trackingtimes[:(-1)])
 nbins = len(trackingtimes)
@@ -74,12 +69,9 @@ headangle = headangle[~whiches]
 binnedspikes = binnedspikes[:,~whiches]
 
 # Select part of data to be able to make X
-path = headangle[offset:offset+T]
+true_path = headangle[offset:offset+T]
 binnedspikes = binnedspikes[:,offset:offset+T]
 binnedspikes = (binnedspikes>0)*1 #Reset to ones 
-
-if (sum(isnan(path)) > 0):
-    print("\nXXXXXXXXX\nXXXXXXXXX\nXXXXXXXXX\nThere are NAN values in path\nXXXXXXXXX\nXXXXXXXXX\nXXXXXXXXX")
 
 N = len(cellnames) #51 with cutoff at 1000 spikes
 print("T:",T)
@@ -87,12 +79,6 @@ print("N:",N)
 print("How many times are there more than one spike:", sum((binnedspikes>1)*1))
 y_spikes = binnedspikes
 print("mean(y_spikes)",mean(y_spikes))
-
-## plot head direction 
-plt.figure(figsize=(10,2))
-plt.plot(path, '.', color='black', markersize=1.)
-plt.savefig(time.strftime("./plots/%Y-%m-%d")+"-headdirection.pdf",format="pdf")
-
 
 def exponential_covariance(t1,t2, sigma, delta):
     distance = abs(t1-t2)
@@ -106,32 +92,7 @@ def gaussian_NONPERIODIC_covariance(x1,x2, sigma, delta):
     distancesquared = (x1-x2)**2
     return sigma * exp(-distancesquared/(2*delta))
 
-####################################################
-## Inference of tuning curves and latent variable ##
-####################################################
-## Using Gaussian Processes ########################
-####################################################
-
-# Require:
-# y_spikes
-# Make initial guess of path
-
-N_observations = T
-x_values_observed = path
-print("Making spatial covariance matrice: Kx_fit at observations")
-Kx_fit_at_observations = np.zeros((N_observations,N_observations))
-for x1 in range(N_observations):
-    for x2 in range(N_observations):
-        Kx_fit_at_observations[x1,x2] = gaussian_periodic_covariance(x_values_observed[x1],x_values_observed[x2], sigma_fit, delta_fit)
-# By adding sigma_epsilon on the diagonal, we assume noise and make the covariance matrix positive semidefinite
-Kx_fit_at_observations = Kx_fit_at_observations  + np.identity(N_observations)*sigma_epsilon_fit
-Kx_fit_at_observations_inverse = np.linalg.inv(Kx_fit_at_observations)
-fig, ax = plt.subplots()
-kx_obs_mat = ax.matshow(Kx_fit_at_observations, cmap=plt.cm.Blues)
-fig.colorbar(kx_obs_mat, ax=ax)
-plt.savefig(time.strftime("./plots/%Y-%m-%d")+"-hd-inference-Kx_fit_at_observations.png")
-
-# Function to check if loglikelihood is increasing
+# Function to check if loglikelihood is increasing for f_tuning_curve
 def loglikelihoodfunction(y_i, f_i):
     if (np.shape(y_i) != np.shape(f_i)):
         print("Size mismatch between y_i and f_i in loglikelihood function!")
@@ -139,62 +100,88 @@ def loglikelihoodfunction(y_i, f_i):
     priorterm = - 0.5*np.dot(f_i, np.dot(Kx_fit_at_observations_inverse, f_i))
     return likelihoodterm + priorterm 
 
-## Finding f hat using Newton's method
-f_tuning_curve = np.zeros(shape(y_spikes)) # Initialize f values
-f_convergence_plot_for_neuron_0 = zeros((100,T)) ## Here we store f values of neuron 0 at every time at each iteration step
-iteration = 0
-while (True):
-    iteration += 1
-    if iteration<4: 
-        learning_rate = 0.1
-    elif iteration<10:
-        learning_rate = 0.1
-    elif iteration<15:
-        learning_rate = 0.1
-    else: 
-        learning_rate = 0.1 
-    old_likelihoods = np.array([loglikelihoodfunction(y_spikes[i],f_tuning_curve[i]) for i in range(N)])
-    for i in range(N): # See equtaion 4.36 in overleaf
-        e_tilde = np.divide(exp(f_tuning_curve[i]), 1 + exp(f_tuning_curve[i]))
-        e_plain_fraction = np.divide(exp(f_tuning_curve[i]), (1 + exp(f_tuning_curve[i]))**2)
-        f_derivative = y_spikes[i] - e_tilde - np.dot(Kx_fit_at_observations_inverse, f_tuning_curve[i])
-        f_hessian = - np.diag(e_plain_fraction) - Kx_fit_at_observations_inverse 
-        # This formulation of Newton's method is slow: 
-        #new_f_i = f_tuning_curve[i] - learning_rate * np.dot(f_derivative,np.linalg.inv(f_hessian))
-        # Instead we do like this (fast): 
-        delta_f_i = np.linalg.solve(f_hessian, - learning_rate * f_derivative)
-        new_f_i = f_tuning_curve[i] + delta_f_i
-        f_tuning_curve[i] = new_f_i
-        if (i==0):
-            f_convergence_plot_for_neuron_0[iteration-1] = f_tuning_curve[i]
-    new_likelihoods = np.array([loglikelihoodfunction(y_spikes[i],f_tuning_curve[i]) for i in range(N)])
-    print("\nNewton Iteration",iteration)
-    print("Biggest likelihood difference", max(new_likelihoods - old_likelihoods)) #The biggest likelihood improvement across all neurons
-    if (max(abs(new_likelihoods - old_likelihoods)) < TOLERANCE):
-        break
-    old_likelihoods = new_likelihoods
 
-plt.figure()
-# Here we plot convergence for neuron 0 
-iterationsteps_to_plot = min(iteration,100)
-colors = [plt.cm.viridis(t) for t in np.linspace(0, 1, T)]
-for t in range(T):
-    plt.plot(f_convergence_plot_for_neuron_0[0:iterationsteps_to_plot,t], color=colors[t])
-#plt.title('f_i_t Convergence, neuron 0, each line is a chosen timestep')
-plt.xlabel("Iteration")
-plt.ylabel("Value of f")
-plt.savefig(time.strftime("./plots/%Y-%m-%d")+"-convergence.pdf",format="pdf")
+def find_f_hat(N, T, path, y_spikes, likelihood):
+    ####################################################
+    ## Inference of tuning curves and latent variable ##
+    ####################################################
+    ## Using Gaussian Processes ########################
+    ####################################################
+
+    Kx_fit_at_observations = np.zeros((T,T))
+    for x1 in range(T):
+        for x2 in range(T):
+            Kx_fit_at_observations[x1,x2] = gaussian_periodic_covariance(x_values_observed[x1],x_values_observed[x2], sigma_fit, delta_fit)
+    # By adding sigma_epsilon on the diagonal, we assume noise and make the covariance matrix positive semidefinite
+    Kx_fit_at_observations = Kx_fit_at_observations  + np.identity(T)*sigma_epsilon_fit
+    Kx_fit_at_observations_inverse = np.linalg.inv(Kx_fit_at_observations)
+
+    ## Finding f hat using Newton's method
+    f_tuning_curve = np.zeros(shape(y_spikes)) # Initialize f values
+    f_convergence_plot_for_neuron_0 = zeros((100,T)) ## Here we store f values of neuron 0 at every time at each iteration step
+    iteration = 0
+    while (True):
+        iteration += 1
+        if iteration<4: 
+            learning_rate = 0.1
+        elif iteration<10:
+            learning_rate = 0.1
+        elif iteration<15:
+            learning_rate = 0.1
+        else: 
+            learning_rate = 0.1 
+        old_likelihoods = np.array([loglikelihoodfunction(y_spikes[i],f_tuning_curve[i]) for i in range(N)])
+        for i in range(N): # See equtaion 4.36 in overleaf
+            # if LIKELIHOOD=="bernoulli"
+            e_tilde = np.divide(exp(f_tuning_curve[i]), 1 + exp(f_tuning_curve[i]))
+            e_plain_fraction = np.divide(exp(f_tuning_curve[i]), (1 + exp(f_tuning_curve[i]))**2)
+            f_derivative = y_spikes[i] - e_tilde - np.dot(Kx_fit_at_observations_inverse, f_tuning_curve[i])
+            f_hessian = - np.diag(e_plain_fraction) - Kx_fit_at_observations_inverse 
+            # This formulation of Newton's method is slow: 
+            #new_f_i = f_tuning_curve[i] - learning_rate * np.dot(f_derivative,np.linalg.inv(f_hessian))
+            # Instead we do like this (fast): 
+            delta_f_i = np.linalg.solve(f_hessian, - learning_rate * f_derivative)
+            new_f_i = f_tuning_curve[i] + delta_f_i
+            f_tuning_curve[i] = new_f_i
+            if (i==0):
+                f_convergence_plot_for_neuron_0[iteration-1] = f_tuning_curve[i]
+        new_likelihoods = np.array([loglikelihoodfunction(y_spikes[i],f_tuning_curve[i]) for i in range(N)])
+        print("\nNewton Iteration",iteration)
+        print("Biggest likelihood difference", max(new_likelihoods - old_likelihoods)) #The biggest likelihood improvement across all neurons
+        if (max(abs(new_likelihoods - old_likelihoods)) < TOLERANCE_f):
+            break
+        old_likelihoods = new_likelihoods
+    return f_tuning_curve
+
+##################
+# Inference of X #
+##################
+X_estimate = np.zeros(T)
+X_loglikelihood_old = -2*TOLERANCE_X
+X_loglikelihood_new = 0
+while abs(X_loglikelihood_new - X_loglikelihood_old) > TOLERANCE_X:
+    X_loglikelihood_old = X_loglikelihood_new
+    f_hat = find_f_hat(N, T, X_estimate, y_spikes, likelihood=LIKELIHOOD)
+
+    
+    
+
+
+
+    X_loglikelihood_new = ...
+
+
 
 #################################################
 # Find posterior prediction of log tuning curve #
 #################################################
 bins = np.linspace(-0.000001, 2.*np.pi+0.0000001, num=X_dim + 1)
 x_grid = 0.5*(bins[:(-1)]+bins[1:])
-f_values_observed = f_tuning_curve
+f_values_observed = f_hat
 
 print("Making spatial covariance matrice: Kx crossover")
-Kx_crossover = np.zeros((N_observations,X_dim))
-for x1 in range(N_observations):
+Kx_crossover = np.zeros((T,X_dim))
+for x1 in range(T):
     for x2 in range(X_dim):
         Kx_crossover[x1,x2] = gaussian_periodic_covariance(x_values_observed[x1],x_grid[x2], sigma_fit, delta_fit)
 fig, ax = plt.subplots()
@@ -240,13 +227,12 @@ h_lower_confidence_limit = np.exp(lower_confidence_limit) / (1 + np.exp(lower_co
 observed_spikes = zeros((N, X_dim))
 for i in range(N):
     for x in range(X_dim):
-        timesinbin = (path>bins[x])*(path<bins[x+1])
+        timesinbin = (X_estimate>bins[x])*(X_estimate<bins[x+1])
         if(sum(timesinbin)>0):
             observed_spikes[i,x] = mean( y_spikes[i, timesinbin] )
         else:
             print("No observations of X between",bins[x],"and",bins[x+1],".")
 
-# Plot only a few
 for n4 in range(N//4):
     plt.figure(figsize=(10,8))
     neuron = np.array([[0,1],[2,3]])
@@ -260,5 +246,12 @@ for n4 in range(N//4):
             plt.plot(x_grid, h_lower_confidence_limit[neuron[i,j],:], "--", color=colors[0])
             plt.ylim(0.,1.)
             plt.title(neuron[i,j]+1)
-    plt.savefig(time.strftime("./plots/%Y-%m-%d")+"hd-fitted-tuning"+str(n4+1)+".pdf",format="pdf")
+    plt.savefig(time.strftime("./plots/%Y-%m-%d")+"hd-fitted-tuning"+str(n4+1)+".png")
 plt.show()
+
+## plot actual head direction together with estimate
+plt.figure(figsize=(10,2))
+plt.plot(true_path, '.', color='black', markersize=1.)
+plt.plot(true_path, '.', color=plt.cm.viridis(0), markersize=1.)
+plt.savefig(time.strftime("./plots/%Y-%m-%d")+"-hd-inference.png")
+
