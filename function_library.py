87a279d09fe08ec9e532175fe39446d0ee70ee7f
evenmm <even.myklebust@gmail.com>
2020-06-22 16:02:27 +0200

    Modular version for easy correcting in all versions of code

---
+++ function_library.py
@@ -0,0 +1,350 @@
+from scipy import *
+import scipy.io
+import scipy.ndimage
+import numpy as np
+import scipy.optimize as spoptim
+import numpy.random
+import matplotlib
+#matplotlib.use('Agg') # When running on cluster, plots cannot be shown and this must be used
+import matplotlib.pyplot as plt
+import time
+import sys
+plt.rc('image', cmap='viridis')
+from scipy import optimize
+numpy.random.seed(13)
+from multiprocessing import Pool
+from sklearn.decomposition import PCA
+from parameter_file import * # where all the parameters are set
+
+
+######################
+# Covariance kernels #
+######################
+
+def squared_exponential_covariance(xvector1, xvector2, sigma, delta):
+    if COVARIANCE_KERNEL_KX == "nonperiodic":
+        distancesquared = scipy.spatial.distance.cdist(xvector1, xvector2, 'sqeuclidean')
+    if COVARIANCE_KERNEL_KX == "periodic":
+        # This handles paths that stretches across anywhere as though the domain is truly periodic
+        # First put every time point between 0 and 2pi
+        xvector1 = xvector1 % (2*np.pi)
+        xvector2 = xvector2 % (2*np.pi)
+        # Then take care of periodicity
+        distancesquared_1 = scipy.spatial.distance.cdist(xvector1, xvector2, 'sqeuclidean')
+        distancesquared_2 = scipy.spatial.distance.cdist(xvector1+2*np.pi, xvector2, 'sqeuclidean')
+        distancesquared_3 = scipy.spatial.distance.cdist(xvector1-2*np.pi, xvector2, 'sqeuclidean')
+        min_1 = np.minimum(distancesquared_1, distancesquared_2)
+        distancesquared = np.minimum(min_1, distancesquared_3)
+    return sigma * exp(-distancesquared/(2*delta**2))
+
+def exponential_covariance(tvector1, tvector2, sigma, delta):
+    absolutedistance = scipy.spatial.distance.cdist(tvector1, tvector2, 'euclidean')
+    return sigma * exp(-absolutedistance/delta)
+
+########################
+# Covariance matrices  #
+########################
+K_t = exponential_covariance(np.linspace(1,T,T).reshape((T,1)),np.linspace(1,T,T).reshape((T,1)), sigma_x, delta_x)
+K_t_inverse = np.linalg.inv(K_t)
+
+######################################
+## Data generation                  ##
+######################################
+K_t_generate = exponential_covariance(np.linspace(1,T,T).reshape((T,1)),np.linspace(1,T,T).reshape((T,1)), sigma_x_generate_path, delta_x_generate_path)
+
+############################
+# Tuning curve definitions #
+############################
+
+if UNIFORM_BUMPS:
+    # Uniform positioning and width:'
+    bumplocations = [min_neural_tuning_X + (i+0.5)/N*(max_neural_tuning_X - min_neural_tuning_X) for i in range(N)]
+    bump_delta_distances = tuning_width_delta * np.ones(N)
+else:
+    # Random placement and width:
+    bumplocations = min_neural_tuning_X + (max_neural_tuning_X - min_neural_tuning_X) * np.random.random(N)
+    bump_delta_distances = tuning_width_delta + tuning_width_delta/4*np.random.random(N)
+
+def bumptuningfunction(x, i, peak_f_offset): 
+    x1 = x
+    x2 = bumplocations[i]
+    delta_bumptuning = bump_delta_distances[i]
+    if COVARIANCE_KERNEL_KX == "periodic":
+        distancesquared = min([(x1-x2)**2, (x1+2*pi-x2)**2, (x1-2*pi-x2)**2])
+    elif COVARIANCE_KERNEL_KX == "nonperiodic":
+        distancesquared = (x1-x2)**2
+    return baseline_f_value + peak_f_offset * exp(-distancesquared/(2*delta_bumptuning))
+
+def offset_function(offset_for_estimate, X_estimate, sigma_n, F_estimate, K_gg, x_grid_induce):
+    offset_estimate = X_estimate + offset_for_estimate
+    return x_posterior_no_la(offset_estimate, sigma_n, F_estimate, K_gg, x_grid_induce)
+
+def scaling_function(scaling_factor, X_estimate, sigma_n, F_estimate, K_gg, x_grid_induce):
+    scaled_estimate = scaling_factor*X_estimate
+    return x_posterior_no_la(scaled_estimate, sigma_n, F_estimate, K_gg, x_grid_induce)
+
+def scale_and_offset_function(scale_offset, X_estimate, sigma_n, F_estimate, K_gg, x_grid_induce):
+    scaled_estimate = scale_offset[0] * X_estimate + scale_offset[1]
+    return x_posterior_no_la(scaled_estimate, sigma_n, F_estimate, K_gg, x_grid_induce)
+    #return just_fprior_term(scaled_estimate)
+
+#########################
+## Likelihood functions #
+#########################
+
+# NEGATIVE Loglikelihood, gradient and Hessian. minimize to maximize. Equation (4.17)++
+def f_loglikelihood_bernoulli(f_i, sigma_n, y_i, K_xg_prev, K_gg): # Psi
+    likelihoodterm = sum( np.multiply(y_i, f_i) - np.log(1+np.exp(f_i))) # Corrected 16.03 from sum( np.multiply(y_i, (f_i - np.log(1+np.exp(f_i)))) + np.multiply((1-y_i), np.log(1- np.divide(np.exp(f_i), 1 + np.exp(f_i)))))
+    priorterm_1 = -0.5*sigma_n**-2 * np.dot(f_i.T, f_i)
+    fT_k = np.dot(f_i, K_xg_prev)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    priorterm_2 = 0.5*sigma_n**-2 * np.dot(np.dot(fT_k, smallinverse), fT_k.T)
+    return - (likelihoodterm + priorterm_1 + priorterm_2)
+def f_jacobian_bernoulli(f_i, sigma_n, y_i, K_xg_prev, K_gg):
+    yf_term = y_i - np.divide(np.exp(f_i), 1 + np.exp(f_i))
+    priorterm_1 = -sigma_n**-2 * f_i
+    kTf = np.dot(K_xg_prev.T, f_i)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    priorterm_2 = sigma_n**-2 * np.dot(K_xg_prev, np.dot(smallinverse, kTf))
+    f_derivative = yf_term + priorterm_1 + priorterm_2
+    return - f_derivative
+def f_hessian_bernoulli(f_i, sigma_n, y_i, K_xg_prev, K_gg):
+    e_tilde = np.divide(np.exp(f_i), (1 + np.exp(f_i))**2)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    f_hessian = - np.diag(e_tilde) - sigma_n**-2 * np.identity(T) + sigma_n**-2 * np.dot(K_xg_prev, np.dot(smallinverse, K_xg_prev.T))
+    return - f_hessian
+
+# NEGATIVE Loglikelihood, gradient and Hessian. minimize to maximize.
+def f_loglikelihood_poisson(f_i, sigma_n, y_i, K_xg_prev, K_gg):
+    likelihoodterm = sum( np.multiply(y_i, f_i) - np.exp(f_i)) 
+    priorterm_1 = -0.5*sigma_n**-2 * np.dot(f_i.T, f_i)
+    fT_k = np.dot(f_i, K_xg_prev)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    priorterm_2 = 0.5*sigma_n**-2 * np.dot(np.dot(fT_k, smallinverse), fT_k.T)
+    return - (likelihoodterm + priorterm_1 + priorterm_2)
+
+def f_jacobian_poisson(f_i, sigma_n, y_i, K_xg_prev, K_gg):
+    yf_term = y_i - np.exp(f_i)
+    priorterm_1 = -sigma_n**-2 * f_i
+    kTf = np.dot(K_xg_prev.T, f_i)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    priorterm_2 = sigma_n**-2 * np.dot(K_xg_prev, np.dot(smallinverse, kTf))
+    f_derivative = yf_term + priorterm_1 + priorterm_2
+    return - f_derivative
+def f_hessian_poisson(f_i, sigma_n, y_i, K_xg_prev, K_gg):
+    e_poiss = np.exp(f_i)
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_xg_prev.T, K_xg_prev))
+    f_hessian = - np.diag(e_poiss) - sigma_n**-2*np.identity(T) + sigma_n**-2 * np.dot(K_xg_prev, np.dot(smallinverse, K_xg_prev.T))
+    return - f_hessian
+
+# L function
+def x_posterior_no_la(X_estimate, sigma_n, F_estimate, K_gg, x_grid_induce): 
+    start = time.time()
+    K_xg = squared_exponential_covariance(X_estimate.reshape((T,1)),x_grid_induce.reshape((N_inducing_points,1)), sigma_f_fit, delta_f_fit)
+    K_gx = K_xg.T
+    stop = time.time()
+    if SPEEDCHECK:
+        print("Speedcheck of L function:")
+        print("Making Kxg            :", stop-start)
+
+    start = time.time()
+    #Kx_inducing = np.matmul(np.matmul(K_xg, K_gg_inverse), K_gx) + sigma_n**2
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_gx, K_xg))
+    # Kx_inducing_inverse = sigma_n**-2*np.identity(T) - sigma_n**-2 * np.matmul(np.matmul(K_xg, smallinverse), K_gx)
+    tempmatrix = np.matmul(np.matmul(K_xg, smallinverse), K_gx)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("Making small/tempmatrx:", stop-start)
+
+    # yf_term ##########
+    ####################
+    #start = time.time()
+    #if LIKELIHOOD_MODEL == "bernoulli": # equation 4.26
+    #    yf_term = sum(np.multiply(y_spikes, F_estimate) - np.log(1 + np.exp(F_estimate)))
+    #elif LIKELIHOOD_MODEL == "poisson": # equation 4.43
+    #    yf_term = sum(np.multiply(y_spikes, F_estimate) - np.exp(F_estimate))
+    #stop = time.time()
+    #if SPEEDCHECK:
+    #    print("yf term               :", stop-start)
+
+    # f prior term #####
+    ####################
+    start = time.time()
+    f_prior_term_1 = sigma_n**-2 * np.trace(np.matmul(F_estimate, F_estimate.T))
+    fK = np.matmul(F_estimate, tempmatrix)
+    fKf = np.matmul(fK, F_estimate.T)
+    f_prior_term_2 = - sigma_n**-2 * np.trace(fKf)
+
+    f_prior_term = - 0.5 * (f_prior_term_1 + f_prior_term_2)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("f prior term          :", stop-start)
+
+    # logdet term ######
+    ####################
+    #logdet_term = - 0.5 * N * np.log(np.linalg.det(Kx_inducing))
+    # smallinverse = np.linalg.inv(np.matmul(K_gx, K_xg) + K_gg*sigma_n**2)
+
+    start = time.time()
+    logDetS1 = np.log(np.linalg.det(np.matmul(K_gx, K_xg) + K_gg*sigma_n**2)) - np.log(np.linalg.det(K_gg)) + (T-N_inducing_points) * np.log(sigma_n**2)
+    logdet_term = - 0.5 * N * logDetS1
+    stop = time.time()
+    if SPEEDCHECK:
+        print("logdet term            :", stop-start)
+
+    # x prior term #####
+    ####################
+    start = time.time()
+    xTKt = np.dot(X_estimate.T, K_t_inverse) # Inversion trick for this too? No. If we don't do Fourier then we are limited by this.
+    x_prior_term = - 0.5 * np.dot(xTKt, X_estimate)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("X prior term          :", stop-start)
+        print("logdet_term", logdet_term)
+        print("f_prior_term", f_prior_term)
+        print("x_prior_term", x_prior_term)
+    posterior_loglikelihood = logdet_term + f_prior_term + x_prior_term #+ yf_term
+    return - posterior_loglikelihood
+
+# Gradient of L 
+def x_jacobian_no_la(X_estimate, sigma_n, F_estimate, K_gg, x_grid_induce):
+    ####################
+    # Initial matrices #
+    ####################
+    start = time.time()
+    K_xg = squared_exponential_covariance(X_estimate.reshape((T,1)),x_grid_induce.reshape((N_inducing_points,1)), sigma_f_fit, delta_f_fit)
+    K_gx = K_xg.T
+    stop = time.time()
+    if SPEEDCHECK:
+        print("\nSpeedcheck of x_jacobian function:")
+        print("Making Kxg            :", stop-start)
+
+    start = time.time()
+    B_matrix = np.matmul(K_gx, K_xg) + (sigma_n**2) * K_gg
+    B_matrix_inverse = np.linalg.inv(B_matrix)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("Making B and B inverse:", stop-start)
+
+    start = time.time()
+    #Kx_inducing = np.matmul(np.matmul(K_xg, K_gg_inverse), K_gx) + sigma_n**2
+    #smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_gx, K_xg))
+    # Kx_inducing_inverse = sigma_n**-2*np.identity(T) - sigma_n**-2 * np.matmul(np.matmul(K_xg, smallinverse), K_gx)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("Making small/tempmatrx:", stop-start)
+
+    ####################
+    # logdet term ######
+    ####################
+    start = time.time()
+
+    ## Evaluate the derivative of K_xg. Row t of this matrix holds the nonzero row of the matrix d/dx_t K_xg
+    d_Kxg = scipy.spatial.distance.cdist(X_estimate.reshape((T,1)),x_grid_induce.reshape((N_inducing_points,1)), lambda u, v: -(u-v)*np.exp(-(u-v)**2/(2*delta_f_fit**2)))
+    d_Kxg = d_Kxg*sigma_f_fit*(delta_f_fit**-2)
+
+    ## Reshape K_gx and K_xg to speed up matrix multiplication
+    K_g_column_tensor = K_gx.T.reshape((T, N_inducing_points, 1)) # Tensor with T depth containing single columns of length N_ind 
+    d_Kx_row_tensor = d_Kxg.reshape((T, 1, N_inducing_points)) # Tensor with T depth containing single rows of length N_ind 
+
+    # Matrix multiply K_gx and d(K_xg)
+    product_Kgx_dKxg = np.matmul(K_g_column_tensor, d_Kx_row_tensor) # 1000 by 30 by 30
+
+    # Sum with transpose
+    trans_sum_K_dK = product_Kgx_dKxg + np.transpose(product_Kgx_dKxg, axes=(0,2,1))
+
+    # Create B^-1 copies for vectorial matrix multiplication
+    B_inv_tensor = np.repeat([B_matrix_inverse],T,axis=0)
+    
+    # Then tensor multiply B^-1 with all the different trans_sum_K_dK
+    big_tensor = np.matmul(B_inv_tensor, trans_sum_K_dK)
+    
+    # Take trace of each individually
+    trace_array = np.trace(big_tensor, axis1=1, axis2=2)
+    
+    # Multiply by - N/2
+    logdet_gradient = - N/2 * trace_array
+
+    stop = time.time()
+    if SPEEDCHECK:
+        print("logdet term            :", stop-start)
+
+    ####################
+    # f prior term ##### (speeded up 10x)
+    ####################
+    start = time.time()
+    fMf = np.zeros((T,N,N))
+
+    ## New hot take:
+    # Elementwise in the sum, priority on things with dim T, AND things that don't need to be vectorized *first*.
+    # Wrap things in from the sides to sandwich the tensor.
+    f_Kx = np.matmul(F_estimate, K_xg)
+    f_Kx_Binv = np.matmul(f_Kx, B_matrix_inverse)
+    #Binv_Kg_f = np.transpose(f_Kx_Binv)
+
+    #d_Kg_column_tensor = np.transpose(d_Kx_row_tensor, axes=(0,2,1))
+
+    # partial derivatives need tensorization
+    # f_dKx = np.matmul(F_estimate, d_Kxg)
+    f_column_tensor = F_estimate.T.reshape((T, N, 1))
+    f_dKx_tensor = np.matmul(f_column_tensor, d_Kx_row_tensor) # (N x N_inducing) matrices  
+    dKg_f_tensor = np.transpose(f_dKx_tensor, axes=(0,2,1))
+
+    f_Kx_Binv_copy_tensor = np.repeat([f_Kx_Binv], T, axis=0)
+    Binv_Kg_f_copy_tensor = np.transpose(f_Kx_Binv_copy_tensor, axes=(0,2,1)) #repeat([Binv_Kg_f], T, axis=0)
+
+    ## A: f dKx Binv Kgx f
+    fMf += np.matmul(f_dKx_tensor, Binv_Kg_f_copy_tensor)
+
+    ## C: - f Kx Binv Kg dKx Binv Kg f
+    Kg_dKx_tensor = np.matmul(K_g_column_tensor, d_Kx_row_tensor)
+    f_Kx_Binv_Kg_dKx_tensor = np.matmul(f_Kx_Binv_copy_tensor, Kg_dKx_tensor)
+    fMf -= np.matmul(f_Kx_Binv_Kg_dKx_tensor, Binv_Kg_f_copy_tensor)
+
+    ## B: - f Kx Binv dKg Kx Binv Kg f
+    dKg_Kx_tensor = np.transpose(Kg_dKx_tensor, axes=(0,2,1))
+    f_Kx_Binv_dKg_Kx_tensor = np.matmul(f_Kx_Binv_copy_tensor, dKg_Kx_tensor)
+    fMf -= np.matmul(f_Kx_Binv_dKg_Kx_tensor, Binv_Kg_f_copy_tensor)
+
+    ## D: f Kx Binv dKg f
+    fMf += np.matmul(f_Kx_Binv_copy_tensor, dKg_f_tensor)
+
+    ## Trace for each matrix in the tensor
+    fMfsum = np.trace(fMf, axis1=1, axis2=2)
+    f_prior_gradient = sigma_n**(-2) / 2 * fMfsum
+
+    stop = time.time()
+    if SPEEDCHECK:
+        print("f prior term          :", stop-start)
+
+    ####################
+    # x prior term #####
+    ####################
+    start = time.time()
+    x_prior_gradient = (-1) * np.dot(X_estimate.T, K_t_inverse)
+    stop = time.time()
+    if SPEEDCHECK:
+        print("X prior term          :", stop-start)
+    ####################
+    x_gradient = logdet_gradient + f_prior_gradient + x_prior_gradient 
+    return - x_gradient
+
+def just_fprior_term(X_estimate): 
+    K_xg = squared_exponential_covariance(X_estimate.reshape((T,1)),x_grid_induce.reshape((N_inducing_points,1)), sigma_f_fit, delta_f_fit)
+    K_gx = K_xg.T
+    #Kx_inducing = np.matmul(np.matmul(K_xg, K_gg_inverse), K_gx) + sigma_n**2
+    smallinverse = np.linalg.inv(K_gg*sigma_n**2 + np.matmul(K_gx, K_xg))
+    # Kx_inducing_inverse = sigma_n**-2*np.identity(T) - sigma_n**-2 * np.matmul(np.matmul(K_xg, smallinverse), K_gx)
+    tempmatrix = np.matmul(np.matmul(K_xg, smallinverse), K_gx)
+
+    # f prior term #####
+    ####################
+    f_prior_term_1 = sigma_n**-2 * np.trace(np.matmul(F_estimate, F_estimate.T))
+    fK = np.matmul(F_estimate, tempmatrix)
+    fKf = np.matmul(fK, F_estimate.T)
+    f_prior_term_2 = - sigma_n**-2 * np.trace(fKf)
+
+    f_prior_term = - 0.5 * (f_prior_term_1 + f_prior_term_2)
+
+    posterior_loglikelihood = f_prior_term #+ logdet_term #+ x_prior_term
+    return - posterior_loglikelihood
---
+++ parameter_file.py
@@ -0,0 +1,81 @@
+from scipy import *
+import scipy.io
+import scipy.ndimage
+import numpy as np
+import scipy.optimize as spoptim
+import numpy.random
+import matplotlib
+#matplotlib.use('Agg') # When running on cluster, plots cannot be shown and this must be used
+import matplotlib.pyplot as plt
+import time
+import sys
+plt.rc('image', cmap='viridis')
+from scipy import optimize
+numpy.random.seed(13)
+from multiprocessing import Pool
+from sklearn.decomposition import PCA
+
+
+############################
+# Parameters for inference #
+############################
+T = 1000 
+N_iterations = 20
+
+global_initial_sigma_n = 2.5 # Assumed variance of observations for the GP that is fitted. 10e-5
+lr = 0.95 # 0.99 # Learning rate by which we multiply sigma_n at every iteration
+
+INFER_F_POSTERIORS = True
+GRADIENT_FLAG = True # Set True to use analytic gradient
+USE_OFFSET_AND_SCALING_AT_EVERY_ITERATION = False
+USE_OFFSET_AND_SCALING_AFTER_CONVERGENCE = True
+TOLERANCE = 1e-6
+X_initialization = "pca" #"true" "ones" "pca" "randomrandom" "randomprior" "linspace"
+# Using ensemble of PCA values
+ensemble_smoothingwidths = [3,5,10] # [1,2,3,5,10,15,20,25,30,40,50,60,70]
+LET_INDUCING_POINTS_CHANGE_PLACE_WITH_X_ESTIMATE = False
+FLIP_AFTER_SOME_ITERATION = False
+FLIP_AFTER_HOW_MANY = 1
+NOISE_REGULARIZATION = False
+SMOOTHING_REGULARIZATION = False
+GIVEN_TRUE_F = False
+SPEEDCHECK = False
+OPTIMIZE_HYPERPARAMETERS = False
+PLOTTING = True
+LIKELIHOOD_MODEL = "poisson" # "bernoulli" "poisson"
+COVARIANCE_KERNEL_KX = "nonperiodic" # "periodic" "nonperiodic"
+TUNINGCURVE_DEFINITION = "bumps" # "triangles" "bumps"
+UNIFORM_BUMPS = True
+PLOT_GRADIENT_CHECK = False
+N_inducing_points = 30 # Number of inducing points. Wu uses 25 in 1D and 10 per dim in 2D
+N_plotgridpoints = 40 # Number of grid points for plotting f posterior only 
+tuning_width_delta = 1.2 # 0.1
+# Peak lambda should not be defined as less than baseline h value
+baseline_lambda_value = 0.5
+baseline_f_value = np.log(baseline_lambda_value)
+tuning_difference_array = [0.01,0.1,0.2,0.3,0.4,0.5,0.75,1,1.25,1.5,1.75,2,2.5,3,3.5,4,5,6,7,8,9]
+peak_lambda_array = [4] #[baseline_lambda_value + tuning_difference_array[i] for i in range(len(tuning_difference_array))]
+seeds = [0] #[0,2,3,4,5,6,8,9,11,12,16,17,18,19,21,22,25,26,28,29] # chosen only so that they cover the entire domain of X for T>=200 and sigma_x=40
+NUMBER_OF_SEEDS = len(seeds)
+print("Number of seeds we average over:", NUMBER_OF_SEEDS)
+sigma_f_fit = 2 #8 # Variance for the tuning curve GP that is fitted. 8
+delta_f_fit = 0.83 # sqrt(0.7) # Scale for the tuning curve GP that is fitted. 0.3
+# Define max and min of neural tuning 
+lower_domain_limit = 0
+upper_domain_limit = 10
+how_many_added_neurons_outside_factor = 0.0 # Just makes it worse. If you must, use 0.1
+min_neural_tuning_X = lower_domain_limit - how_many_added_neurons_outside_factor*(upper_domain_limit - lower_domain_limit)
+max_neural_tuning_X = upper_domain_limit + how_many_added_neurons_outside_factor*(upper_domain_limit - lower_domain_limit)
+min_inducing_point = lower_domain_limit
+max_inducing_point = upper_domain_limit
+# Neural density:
+N = int((1+2*how_many_added_neurons_outside_factor)*100) # 100 with peaks in tuning area and 40 with tails coming in from each side
+# For inference:
+sigma_x = 40 # Variance of X for inference matrix K_t 
+delta_x = 100 # Scale of X for inference matrix K_t
+# Generative parameters for X path:
+KEEP_PATH_INSIDE_DOMAIN_BY_FOLDING = True # Stop path from going outside defined domain with neurons
+SCALE_UP_PATH_TO_COVER_DOMAIN = False # If True, the generated path is scaled up after being generated
+sigma_x_generate_path = 40 # Variance for path generation. Set high enough so the path reaches max and min of tuning area
+delta_x_generate_path = 100 # Scale for path generation.
+jitter_term = 1e-5
